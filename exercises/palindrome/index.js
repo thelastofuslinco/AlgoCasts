f4113d64c0315bbd3bb81b289b11e4b0bac6f404
Lincoln Pinto Duarte <lincolnskyrim_99@hotmail.com>
2026-05-30 13:14:30 -0300

    feat: palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+ let left = 0;
+ let right = str.length -1
+
+ while(left < right) {
+    if(str[left] != str[right]) return false
+    
+    left +=1
+    right -=1
+ }
+ return true
+}
 
 module.exports = palindrome;
