07ad03ef3327fc567b21746c8ff5864c6a77a5ae
Lincoln Pinto Duarte <lincolnskyrim_99@hotmail.com>
2026-05-30 15:30:59 -0300

    feat: max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {}
+    let max = {value: 0};
+
+    for(char of str){
+        if(!chars[char]) chars[char] = 0
+        chars[char] += 1
+
+        if(chars[char] > max.value) {
+            max.value = chars[char]
+            max.key = char
+        }
+    }
+
+    return max.key
+}
 
 module.exports = maxChar;
