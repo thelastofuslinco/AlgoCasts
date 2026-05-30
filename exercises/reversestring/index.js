18ca1e4be782789f7c7b37c57a93a32a581319d1
Lincoln Pinto Duarte <lincolnskyrim_99@hotmail.com>
2026-05-29 21:49:59 -0300

    first commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let left = 0;
+    let right = str.length - 1;
+    const arr = str.split('')
+
+    while(left < right){
+        const aux = arr[left]
+        arr[left] = arr[right]
+        arr[right] = aux
+        left += 1
+        right -= 1
+    }
+
+    return arr.join('')
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//    return str.split('').reduce((prev, curr)=>{
+//     return curr + prev
+//    },'')
+// }
