478532aa8b1e3acbccb1020be1c73b4585e25672
Lincoln Pinto Duarte <lincolnskyrim_99@hotmail.com>
2026-05-30 13:50:55 -0300

    feat: reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,30 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const  newString = n.toString()
+    const arr = newString.split('')
+
+    let left = 0
+    let right = arr.length -1
+
+    while(left < right) {
+        const aux = arr[left]
+        arr[left] = arr[right]
+        arr[right] = aux
+        
+        left ++
+        right --
+    }
+
+    const sign = Math.sign(n)
+
+    if(sign === 1 || sign === 0){
+        return Number(arr.join(''))
+    }
+
+    arr.pop()
+    return - Number(arr.join(''))
+}
 
 module.exports = reverseInt;
