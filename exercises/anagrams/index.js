08b919cc5d1c7a3df62727b5733302d28d4de7ee
Lincoln Pinto Duarte <lincolnskyrim_99@hotmail.com>
2026-06-03 12:50:56 -0300

    feat: anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,26 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  if (stringA.length !== stringB.length) return false;
+  const charMapA = {};
+  const charMapB = {};
+
+  for (let index = 0; index < stringA.length; index++) {
+    const element = stringA[index].toLowerCase();
+    if (!charMapA[element]) charMapA[element] = 0;
+    charMapA[element] += 1;
+
+    const elementB = stringB[index].toLowerCase();
+    if (!charMapB[elementB]) charMapB[elementB] = 0;
+    charMapB[elementB] += 1;
+  }
+
+  for (let key in charMapA) {
+    if (charMapA[key] !== charMapB[key]) return false;
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
