e3bf2b5ee913f58b6fd83f45201dbe9486f3e98f
Lincoln Pinto Duarte <lincolnskyrim_99@hotmail.com>
2026-06-11 15:27:00 -0300

    feat: level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const array = [root, "s"];
+  const counters = [0];
+
+  while (array.length > 1) {
+    const node = array.shift();
+
+    if (node === "s") {
+      counters.push(0);
+      array.push("s");
+    } else {
+      array.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
